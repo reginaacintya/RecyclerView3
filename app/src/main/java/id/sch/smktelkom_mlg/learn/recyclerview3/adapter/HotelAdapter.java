998f590dc0f2e237648bd6cfdf413b30a186b2f2
reginaacintya <regina>
2016-11-03 13:40:11 +0700

    RecyclerView View Detail

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
@@ -1,5 +1,7 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3.adapter;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +20,7 @@ import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
 public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>{
 
+    IHotelAdapter mIHotelApapter;
     ArrayList<Hotel> hotelList;
 
     public HotelAdapter(ArrayList<Hotel> hotelList)
@@ -37,7 +40,7 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>{
         Hotel hotel = hotelList.get(position);
         holder.tvJudul.setText(hotel.judul);
         holder.tvDeskripsi.setText(hotel.deskripsi);
-        holder.ivFoto.setImageDrawable(hotel.foto);
+        holder.ivFoto.setImageURI(Uri.parse(hotel.foto));
     }
 
     @Override
@@ -59,6 +62,26 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>{
             ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
             tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
             tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+
+            itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelApapter.doClick(getAdapterPosition());
+                }
+            });
         }
+    }
+
+    public interface IHotelAdapter
+    {
+        void doClick(int pos);
+    }
+
+    public HotelAdapter(Context context, ArrayList<Hotel> hotelList)
+    {
+        this.hotelList = hotelList;
+        mIHotelApapter = (IHotelAdapter) context;
     }
 }
